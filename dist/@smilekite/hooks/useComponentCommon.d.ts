a3947024d04439df373a0baba7c9c1a1f307597f
kite <2257440643@qq.com>
2024-09-22 20:32:32 +0800

    fix:  build output name

--- dist/@smilekite/hooks/useComponentCommon.d.ts
+++
@@ -1,8 +0,0 @@
-import { CommonComponentProps } from '../defaultProps';
-declare const useComponentCommon: (props: Readonly<Partial<CommonComponentProps & {
-    isEditing: boolean;
-}>>, picks: string[]) => {
-    styleProps: import("vue").ComputedRef<any>;
-    handleClick: () => void;
-};
-export default useComponentCommon;
