a0964ff9df74a96688b7fda686604f3f65f3d1ec
kite <2257440643@qq.com>
2024-11-10 12:36:08 +0800

    chore: off no-explicit-any

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,9 @@ module.exports = {
 
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-explicit-any': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-non-null-assertion': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   },
 
   overrides: [
